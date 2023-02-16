0f2a1c3efc49e002a2da2593b102216ffe19ddcf
Dele Omotosho <dele@authoritywit.com>
2023-02-16 11:39:53 -0500

    completes displaying the items in HTML

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-// 1. Bring in all DOM elements (#search-box & #images)
+//# 1. Bring in all DOM elements (#search-box & #images)
 const searchBoxEl = document.querySelector('#search-box');
 const imageResults = document.querySelector('#images');
 
 // console.log(searchBoxEl)
 // console.log(imageResults)
 
-// 2a. listen for the "Enter" key press on the #search-box
+//# 2a. listen for the "Enter" key press on the #search-box
 searchBoxEl.addEventListener('keypress', (event) => {
-    //2b.  listen the "Enter" on the keyboard
+    //# 2b.  listen the "Enter" on the keyboard
     if (event.key === 'Enter') {
 
-        console.log(`You just HIT the Enter key.....`)
+        // console.log(`You just HIT the Enter key.....`)
         // console.log(event.key);
         // console.log(event.code);
-        // 3. When a user hits enter on their keyboard, send an API request to NASA to get all images
+
+
+        //# 3. When a user hits enter on their keyboard, send an API request to NASA to get all images
         // https://images-api.nasa.gov/search?q={q}
         //     console.log( event.target )
-        console.log("Value user typed:", event.target.value)
+        // console.log("Value user typed:", event.target.value)
         //
         // // use the value the user types to send an API GET  request...
         fetch(`https://images-api.nasa.gov/search?q=${event.target.value}`)
@@ -31,7 +33,12 @@ searchBoxEl.addEventListener('keypress', (event) => {
                 // console.log("image description:", images[0].links[0].href)
 
                 // 4. when the images comes back, display to user in the #images by .appendChild()
-                displayImage(images[0])
+                const displayItems = images.slice(0, 8);
+
+                imageResults.innerHTML = ''
+                displayItems.forEach( image => displayImage(image) );
+
+                // displayImage(images[0])
             })
             .catch(err => {
                 console.log(err);
