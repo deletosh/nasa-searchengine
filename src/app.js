59fead40a1789ebfa9e5552a0b5f471e237156a5
Dele Omotosho <dele@authoritywit.com>
2023-02-16 11:30:48 -0500

    completes displaying one image from NASA's api

--- src/app.js
+++ src/app.js
@@ -17,23 +17,36 @@ searchBoxEl.addEventListener('keypress', (event) => {
         // https://images-api.nasa.gov/search?q={q}
         //     console.log( event.target )
         console.log("Value user typed:", event.target.value)
-
-        // use the value the user types to send an API GET  request...
+        //
+        // // use the value the user types to send an API GET  request...
         fetch(`https://images-api.nasa.gov/search?q=${event.target.value}`)
-            .then( response => response.json() )
-            .then( data => {
+            .then(response => response.json())
+            .then(data => {
 
                 // same the images key in this array
                 const images = data.collection.items
 
-                console.log("image title:", images[0].data[0].title)
-                console.log("image description:", images[0].data[0].description)
-                console.log("image description:", images[0].links[0].href)
+                // console.log("image title:", images[0].data[0].title)
+                // console.log("image description:", images[0].data[0].description)
+                // console.log("image description:", images[0].links[0].href)
 
                 // 4. when the images comes back, display to user in the #images by .appendChild()
+                displayImage(images[0])
             })
-
-
+            .catch(err => {
+                console.log(err);
+            })
     }
-
 });
+
+function displayImage(image) {
+    const div = document.createElement('div');
+
+    div.classList.add('p-6', 'bg-amber-300', 'rounded-lg')
+    div.innerHTML = `
+     <img src="${image.links[0].href}">
+      <p class="font-bold text-lg">${image.data[0].title}</p>
+    <p class="text-xs">${image.data[0].description}</p>
+    `
+    imageResults.appendChild(div);
+}
