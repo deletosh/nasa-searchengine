e986bb307cdecc46d3bd199b5189b906177d4f13
Dele Omotosho <dele@authoritywit.com>
2023-02-21 11:06:29 -0500

    refactors to use async/await

--- src/app.js
+++ src/app.js
@@ -2,50 +2,20 @@
 const searchBoxEl = document.querySelector('#search-box');
 const imageResults = document.querySelector('#images');
 
-// console.log(searchBoxEl)
-// console.log(imageResults)
+// async/await [syntax]
+// const allImages = await fetch(`https://images-api.nasa.gov/search?q=${event.target.value}`);
+//
+// console.log(allImages);
+// console.log('hello');
+
+async function getNASAImages(userInput) {
+    const response = await fetch(`https://images-api.nasa.gov/search?q=${userInput}`);
+    const data = await response.json()
+
+    return data.collection.items;
+}
 
 //# 2a. listen for the "Enter" key press on the #search-box
-searchBoxEl.addEventListener('keypress', (event) => {
-    //# 2b.  listen the "Enter" on the keyboard
-    if (event.key === 'Enter') {
-
-        // console.log(`You just HIT the Enter key.....`)
-        // console.log(event.key);
-        // console.log(event.code);
-
-
-        //# 3. When a user hits enter on their keyboard, send an API request to NASA to get all images
-        // https://images-api.nasa.gov/search?q={q}
-        //     console.log( event.target )
-        // console.log("Value user typed:", event.target.value)
-        //
-        // // use the value the user types to send an API GET  request...
-        fetch(`https://images-api.nasa.gov/search?q=${event.target.value}`)
-            .then(response => response.json())
-            .then(data => {
-
-                // same the images key in this array
-                const images = data.collection.items
-
-                // console.log("image title:", images[0].data[0].title)
-                // console.log("image description:", images[0].data[0].description)
-                // console.log("image description:", images[0].links[0].href)
-
-                // 4. when the images comes back, display to user in the #images by .appendChild()
-                const displayItems = images.slice(0, 8);
-
-                imageResults.innerHTML = ''
-                displayItems.forEach( image => displayImage(image) );
-
-                // displayImage(images[0])
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-});
-
 function displayImage(image) {
     const div = document.createElement('div');
 
@@ -56,4 +26,20 @@ function displayImage(image) {
     <p class="text-xs">${image.data[0].description}</p>
     `
     imageResults.appendChild(div);
+
+    //DOM Tree <<<
 }
+
+searchBoxEl.addEventListener('keypress', async (event) => {
+    //# 2b.  listen the "Enter" on the keyboard
+
+    if (event.key === 'Enter') {
+        //# 3. When a user hits enter on their keyboard, send an API request to NASA to get all images
+        const allNasaImages = await getNASAImages(event.target.value);
+
+        const imagesToShow = allNasaImages.slice(0, 8);
+        imagesToShow.forEach(image => displayImage(image));
+
+    }
+
+});
